732e8a18d2a94992795b7d5449fa12706928b2fe
RheumLynx <josequisab@gmail.com>
2024-10-01 22:50:53 +0200

    Cambios app.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from api import create_api
 from config import Config
 import os
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../frontend')
     CORS(app)  # Esto habilita CORS para toda la aplicación
     app.config.from_object(config_class)
     
@@ -18,9 +18,13 @@ def create_app(config_class=Config):
 
     create_api(app)
 
-    @app.route('/')
-    def index():
-        return "Bienvenido a la API de análisis de resultados de laboratorio"
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve_frontend(path):
+        if path != "" and os.path.exists(app.static_folder + '/' + path):
+            return send_from_directory(app.static_folder, path)
+        else:
+            return send_from_directory(app.static_folder, 'index.html')
 
     return app
 
@@ -30,4 +34,3 @@ if __name__ == '__main__':
 
 # Crear una instancia de la aplicación para Gunicorn
 app = create_app()
-    
